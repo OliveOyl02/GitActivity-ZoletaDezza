70fbfd7557db9a10f0ea51b332cd11f9b5d5963b
ZoletaDezza <zoletadezza101@gmail.com>
2022-03-29 22:04:48 +0800

    My Educational Background added

--- GitActivity-ZoletaDezza/git-python-zoleta.py
+++ GitActivity-ZoletaDezza/git-python-zoleta.py
@@ -1,3 +1,7 @@
 print("Dezza Antoinette Zoleta");
 print("Bicas-Bicas,Buenavista, Marinduque");
 print("November 2, 2000");
+print("Marinduque State College");
+print("Bachelor of  Science in Information Technology");
+print("2nd year");
+print("Section E")
